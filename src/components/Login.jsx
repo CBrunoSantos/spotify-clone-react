537991ec9879aa951c3790c8fffb148d93366f21
Carlos Bruno <49172271+CBrunoSantos@users.noreply.github.com>
2022-10-19 11:42:14 -0300

    parte 2 tela login: usando o acesso do token ao spotify

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,24 @@ import styled from "styled-components";
 export default function Login() {
 
   const handleClick = () => {
-    // alert("ouvindo musica");
-  }
+    const client_id = "4da1a470015342eebf2b20bccfde35c7";
+    const redirect_uri = "http://localhost:3000/";
+    const api_uri = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-email",
+      "user-read-private",
+      "user-modify-playback-state",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "user-read-recently-played",
+      "user-read-playback-position",
+      "user-top-read"
+    ];
+
+    window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+      " "
+    )}&response_type=token&show_dialog=true`;
+  };
 
   return (
   <Container>
